e99e3a10fb8cdff614a928cec79ab93a9a08bce7
scpaes <scp2@cin.ufpe.br>
2021-07-23 00:40:14 -0300

    Create project

---
+++ setup/settings/develop.py
@@ -0,0 +1,14 @@
+from .settings import *
+
+DEBUG = True
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5437',
+    }
+}
